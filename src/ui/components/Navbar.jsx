f3b67effa521e4ba4779e12b91e096499ecb6feb
jojupacr5 <joseph.paucar@gmail.com>
2024-08-11 13:38:47 -0500

    Creacion de un nuevo route que solo se usa para los heroes y separando el login del layout normal una vez logeado

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,16 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate('/login', {
+      replace: true
+    });
+  }
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
         
@@ -28,6 +37,13 @@ export const Navbar = () => {
           >
             DC
           </NavLink>
+
+          <NavLink 
+            className={({ isActive }) => `nav-item nav-link ${ isActive ? 'active' : ''}`}
+            to="/search"
+          >
+            Search
+          </NavLink>
         </div>
       </div>
 
@@ -39,6 +55,7 @@ export const Navbar = () => {
 
           <button
             className='nav-item nav-link btn'
+            onClick={handleLogout}
           >
             Logout
           </button>
